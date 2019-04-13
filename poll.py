d8e5528fd8a95c0a3ef14ce57cfaf4ef3b01465e
Tom Casavant <tfcasavant@gmail.com>
2019-04-13 00:09:02 -0400

    Added error verification

--- poll.py
+++ poll.py
@@ -18,16 +18,29 @@ class Poll:
         self.choices = choices
         self.votes = [0] * len(choices)  # initialize all votes to zero
         self.voters = []
+        self.active = True
 
     def vote(self, choice, user_id):
         self.votes[choice-1] += 1
         self.voters.append(user_id)
+
+    def isAvailable(self, choice):
+        return choice <= len(self.choices)
+
+    def hasVoted(self, user):
+        return user in self.voters
+
+    def isActive(self):
+        return self.active
 
     def get_results(self):
         results = "<br />".join(
             [f"{choice}: {self.votes[i]}" for i, choice in enumerate(self.choices)]
         )
         return results
+
+    def end_poll(self):
+        self.active = False
 
 
 class PollPlugin(Plugin):
@@ -63,12 +76,24 @@ class PollPlugin(Plugin):
     @command.argument("choice", pass_raw=True, required=True)
     async def handler(self, evt: MessageEvent, choice: int) -> None:
         await evt.mark_read()
-        if evt.sender in self.currentPoll.voters:
+        if self.currentPoll.hasVoted(evt.sender):
             await evt.reply("You've already voted in this poll")
+        elif not self.currentPoll.isActive():
+            await evt.reply("I'm sorry this poll has already ended")
         else:
-            self.currentPoll.vote(int(choice), evt.sender)
+            if self.currentPoll.isAvailable(int(choice)):
+                self.currentPoll.vote(int(choice), evt.sender)
+            else:
+                await evt.reply("You must enter a valid choice")
 
     @poll.subcommand("results")
     async def handler(self, evt: MessageEvent) -> None:
         await evt.mark_read()
-        await evt.reply(self.currentPoll.get_results())
+        await evt.reply(self.currentPoll.get_results(), html_in_markdown=True)
+
+
+    @poll.subcommand("end")
+    async def handler(self, evt: MessageEvent) -> None:
+        await evt.mark_read()
+        self.currentPoll.end_poll()
+        await evt.reply("This poll is now over. Type !poll results to see the results.")
